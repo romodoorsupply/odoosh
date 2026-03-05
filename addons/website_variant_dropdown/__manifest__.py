bb66be2887f3c686e76a411b9485c716515ab1a1
doubleprince-code <adesojiade1@gmail.com>
2026-03-05 07:27:01 -0800

    Custom module setup for installation v1

--- addons/website_variant_dropdown/__manifest__.py
+++ addons/website_variant_dropdown/__manifest__.py
@@ -1,7 +1,14 @@
 {
     "name": "Website Variant Dropdown",
-    "version": "19.01",
-    "depends": ["website_sale"],
+    "version": "19.01.03",
+    "summary": "Advanced Online Door Expert",
+    "category": "Website",
+    "author": "Romodoor",
+    'license': 'LGPL-3',
+    "depends": [
+            'website',
+            'website_sale'
+        ],
     "data": [
         "views/product_variant_templates.xml",
     ],
@@ -10,5 +17,5 @@
             "website_variant_dropdown/static/src/js/variant_dropdown.js",
         ],
     },
-    "installable": True,
+   'installable': True,
 }
