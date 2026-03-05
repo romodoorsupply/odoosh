164799642b2af4ca40583d9d6770b76bc40979d1
doubleprince-code <adesojiade1@gmail.com>
2026-03-05 07:20:30 -0800

    Custom module setup for installation

---
+++ addons/website_variant_dropdown/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Website Variant Dropdown",
+    "version": "19.01",
+    "depends": ["website_sale"],
+    "data": [
+        "views/product_variant_templates.xml",
+    ],
+    "assets": {
+        "web.assets_frontend": [
+            "website_variant_dropdown/static/src/js/variant_dropdown.js",
+        ],
+    },
+    "installable": True,
+}
